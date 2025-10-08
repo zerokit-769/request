7cd002aed625c8aa2c1ecc4ab416984c7527f6f0
zerokit-769 <haidliralimutan@gmail.com>
2025-10-09 04:05:38 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'sa-up01.jaco.live'
+      url.hostname = '2.round.web.id'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }

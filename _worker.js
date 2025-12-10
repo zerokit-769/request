09f3374b7b9effdec4a185974b021363b1d290e5
zerokit-769 <haidliralimutan@gmail.com>
2025-12-11 04:14:11 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'railwayv2ray-production-e1f0.up.railway.app'
+      url.hostname = 'institutes-richards-cold-threats.trycloudflare.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
